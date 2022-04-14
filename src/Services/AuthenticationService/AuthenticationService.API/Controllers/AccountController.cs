78cb395ecab39787be660ed84bca0e8539f2772e
yuryshev <stanislav.yuryshev@gmail.com>
2022-04-14 03:23:46 +0300

    Init AuthenticationService.API

---
+++ src/Services/AuthenticationService/AuthenticationService.API/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthenticationService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("login/{username}")]
+        public IActionResult Login(string username)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var jwt = new JwtSecurityToken(
+                issuer: "issuer",
+                audience: "audience",
+                claims: claims,
+                expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("HA65GAHF68769GFGf5678")), SecurityAlgorithms.HmacSha256)
+                );
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(jwt));
+        }
+    }
+}
